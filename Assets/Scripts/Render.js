ab471749f5ba2c2e1c8f2e5896dd584f2435c65a
supermiu <jeffersonlee2000@gmail.com>
2014-04-03 18:06:05 -0700

    The base
    the bottom stuff

---
+++ Assets/Scripts/Render.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+var speed : float;
+var jumpheight : float;
+function Start () {
+
+}
+
+function Update () {
+	transform.position += Vector3(0,0,Input.GetAxis("Vertical") / speed);
+	if(Input.GetKeyDown(KeyCode.Space)) {
+		if(transform.position.y < 2.5) {
+			transform.position.y += jumpheight;
+		}
+	}
+}
