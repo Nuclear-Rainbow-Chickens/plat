6da0faedb785b73cfe14f932d52c471ffb3ed794
hehaho101 <aaronhl101@gmail.com>
2014-04-11 17:41:20 -0700

    Aaron's on!
    editing from his account

---
+++ Assets/Scripts/Restart.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var playertrans : Transform;
+var restartpos : Vector3;
+function Start () {
+	restartpos = playertrans.position;
+}
+
+function Update () {
+	if(playertrans.position.y < -20) {
+		transform.position = restartpos;
+	}
+}
