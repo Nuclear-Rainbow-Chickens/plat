8a2d86d7967bbf55e17f9190900a8b70034e0466
supermiu <jeffersonlee2000@gmail.com>
2014-04-12 12:24:53 -0700

    new a lot of stuff

---
+++ Assets/Scripts/Restart.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var firstpos : Vector3;
+function Start () {
+	firstpos = transform.position;
+}
+
+function Update () {
+	if(transform.position.y < -20) {
+		transform.position = Vector3(firstpos.x,100,firstpos.z);
+		rigidbody.velocity = Vector3.zero;
+	}
+}
