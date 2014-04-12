7d0b53aec19555c4e71c89b9fdfadfddbbb6e73d
supermiu <jeffersonlee2000@gmail.com>
2014-04-12 16:07:15 -0700

    friction
    f = m * a

---
+++ Assets/Scripts/Redo.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+var firstpos : Vector3;
+var restart = false;
+var max_vel : float;
+function Start () {
+	firstpos = transform.position;
+}
+
+function Update () {
+	if(transform.position.y < -20) {
+		transform.position = Vector3(firstpos.x,100,firstpos.z);
+		rigidbody.velocity = Vector3.zero;
+		restart = true;
+	}
+	if((rigidbody.velocity.y > -81) && (restart == true)) {
+		rigidbody.velocity.y = max_vel * -1;
+	}
+}
+function OnCollisionEnter(floor : Collision) {
+	if(floor.collider.gameObject.tag == "ground") {	
+		restart = false;
+		return;
+	}
+}
