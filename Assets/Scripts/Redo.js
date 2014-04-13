5dfb0a8ca22b1b88a7adfc791fa73c465fdb94f4
supermiu <jeffersonlee2000@gmail.com>
2014-04-13 11:32:45 -0700

    Shooting!
    pew pew

--- Assets/Scripts/Redo.js
+++ Assets/Scripts/Redo.js
@@ -1,15 +1,18 @@
 ﻿#pragma strict
 var firstpos : Vector3;
+var firstrot : Quaternion;
 var restart = false;
 var max_vel : float;
 function Start () {
 	firstpos = transform.position;
+	firstrot = transform.rotation;
 }
 
 function Update () {
 	if(transform.position.y < -20) {
 		transform.position = Vector3(firstpos.x,100,firstpos.z);
 		rigidbody.velocity = Vector3.zero;
+		transform.rotation = firstrot;
 		restart = true;
 	}
 	if((rigidbody.velocity.y > -81) && (restart == true)) {
---
+++ Assets/Scripts/Shoot.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+var firerate : float;
+var bulletspeed : float;
+var canfire = true;
+var bullet : GameObject;
+function Start () {
+
+}
+
+function Update () {
+	if((Input.GetKey(KeyCode.LeftShift)) && (canfire != false)) {
+		var clone : GameObject;
+		clone = Instantiate(bullet,Vector3(transform.position.x,transform.position.y + 2,transform.position.z),transform.rotation);
+		clone.rigidbody.velocity = transform.TransformDirection(Vector3.forward * bulletspeed);
+		canfire = false;
+		refire();
+	}
+}
+function refire() {
+	yield WaitForSeconds(firerate);
+	canfire = true;
+}
